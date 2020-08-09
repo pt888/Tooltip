95a032d5f1c2b6e7bb4923e1ad2681aa7d0c43df
Priya Kumari <priya.tiwary20@gmail.com>
2020-08-09 20:07:48 +0530

    Updates

---
+++ precache-manifest.619bd991b430ff5fe5c6144aa115006a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918ca40b2e32b9bcb0c54242fbc531c5",
+    "url": "/Tooltip/index.html"
+  },
+  {
+    "revision": "bb638e39f53c6586e666",
+    "url": "/Tooltip/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e6eb14cf3087686a01c6",
+    "url": "/Tooltip/static/css/main.b9e38540.chunk.css"
+  },
+  {
+    "revision": "bb638e39f53c6586e666",
+    "url": "/Tooltip/static/js/2.11fff525.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/Tooltip/static/js/2.11fff525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6eb14cf3087686a01c6",
+    "url": "/Tooltip/static/js/main.d9dfce5c.chunk.js"
+  },
+  {
+    "revision": "0d5c63781616c6e8a024",
+    "url": "/Tooltip/static/js/runtime-main.33c194d2.js"
+  }
+]);
